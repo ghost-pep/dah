986d2b308852442a3ae2e62c2bd3ec1ed6608b43
Daniel Pagan <deapagan@gmail.com>
2023-06-04 18:17:40 -0700

    counter example

--- Program.fs
+++ Program.fs
@@ -1,2 +1,47 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿namespace Dah
+
+open Avalonia
+open Avalonia.Controls
+open Avalonia.Themes.Fluent
+open Elmish
+open Avalonia.FuncUI.Hosts
+open Avalonia.FuncUI
+open Avalonia.FuncUI.Elmish
+open Avalonia.Controls.ApplicationLifetimes
+
+type MainWindow() as this =
+    inherit HostWindow()
+
+    do
+        base.Title <- "Counter Example"
+        base.Height <- 400.0
+        base.Width <- 400.0
+
+        Elmish.Program.mkSimple Counter.init Counter.update Counter.view
+        |> Program.withHost this
+        |> Program.withConsoleTrace
+        |> Program.run
+
+type App() =
+    inherit Application()
+
+    override this.Initialize() =
+        this.Styles.Add(FluentTheme())
+        this.RequestedThemeVariant <- Styling.ThemeVariant.Dark
+
+    override this.OnFrameworkInitializationCompleted() =
+        match this.ApplicationLifetime with
+        | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime ->
+            let mainWindow = MainWindow()
+            desktopLifetime.MainWindow <- mainWindow
+        | _ -> ()
+
+module Program =
+
+    [<EntryPoint>]
+    let main (args: string []) =
+        AppBuilder
+            .Configure<App>()
+            .UsePlatformDetect()
+            .UseSkia()
+            .StartWithClassicDesktopLifetime(args)
