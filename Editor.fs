19f53b1b49b84aac0cdf91e82ce76b0e18c0ebbf
Daniel Pagan <deapagan@gmail.com>
2023-06-04 22:58:15 -0700

    a buggy separation of modes :(

--- Editor.fs
+++ Editor.fs
@@ -4,14 +4,55 @@ open Avalonia.FuncUI.DSL
 
 module Editor =
     open Avalonia.Controls
+    open Avalonia.FuncUI.Types
     open Avalonia.Layout
+    open Avalonia.Input
+    open Avalonia.Interactivity
 
     let title = "Dah Editor"
 
-    let init () = ()
+    type Mode =
+        | Normal
+        | Insert
 
-    let update () () = ()
+    type State = { mode: Mode; buffer: string }
 
-    let view () (dispatch) =
-        TextBox.create [ TextBox.text "HI"
-                         TextBox.onTextChanged (fun newText -> ()) ]
+    let init () = { mode = Normal; buffer = "" }
+
+    type Msg =
+        | Nothing
+        | EnterInsert
+        | EnterNormal
+        | BufferUpdate of string
+
+    let update (msg: Msg) (state: State) : State =
+        match msg with
+        | Nothing -> state
+        | EnterInsert -> { state with mode = Insert }
+        | EnterNormal -> { state with mode = Normal }
+        | BufferUpdate b -> { state with buffer = b }
+
+    let handleNormalKeyDown (keyEvent: KeyEventArgs) =
+        match keyEvent.Key with
+        | Key.I ->
+            keyEvent.Handled <- true
+            EnterInsert
+        | x -> Nothing
+
+    let handleInsertKeyDown (keyEvent: KeyEventArgs) =
+        match keyEvent.Key with
+        | Key.Escape ->
+            keyEvent.Handled <- true
+            EnterNormal
+        | _ -> Nothing
+
+    let handleKeyDown (mode: Mode) (keyEvent: KeyEventArgs) =
+        match mode with
+        | Normal -> handleNormalKeyDown keyEvent
+        | Insert -> handleInsertKeyDown keyEvent
+
+    let view (state: State) (dispatch: Msg -> unit) : IView =
+        TextBox.create [ TextBox.text state.buffer
+                         TextBox.onKeyDown (handleKeyDown state.mode >> dispatch)
+                         TextBox.onTextChanged (BufferUpdate >> dispatch)
+                         TextBox.isReadOnly (state.mode = Normal) ]
